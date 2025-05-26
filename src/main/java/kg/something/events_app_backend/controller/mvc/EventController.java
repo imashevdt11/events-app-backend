f8a3bf566884f4bd9566815a5413c6f7e1504121
imashevdt11 <imashevdt@gmail.com>
2025-05-26 08:48:53 +0600

    Add page with events in admin panel

---
+++ src/main/java/kg/something/events_app_backend/controller/mvc/EventController.java
@@ -0,0 +1,32 @@
+package kg.something.events_app_backend.controller.mvc;
+
+import kg.something.events_app_backend.dto.EventListDto;
+import kg.something.events_app_backend.service.EventService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/events")
+public class EventController {
+
+    private final EventService service;
+
+    public EventController(EventService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public String getAllEvents(Model model) {
+        try {
+            List<EventListDto> events = service.getAllEvents();
+            model.addAttribute("events", events);
+            return "event_list";
+        } catch (Exception e) {
+            return "error";
+        }
+    }
+}
